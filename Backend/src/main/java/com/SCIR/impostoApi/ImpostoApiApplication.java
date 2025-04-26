5e599006684f7861679ba030de1df2896513c129
Amauri1327 <amauri1816@hotmail.com>
2025-04-26 13:21:19 -0300

    Primeiro commit

---
+++ Backend/src/main/java/com/SCIR/impostoApi/ImpostoApiApplication.java
@@ -0,0 +1,13 @@
+package com.SCIR.impostoApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpostoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpostoApiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/SCIR/impostoApi/ImpostoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SCIR.impostoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpostoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
