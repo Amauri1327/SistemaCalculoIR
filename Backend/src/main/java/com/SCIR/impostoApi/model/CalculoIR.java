82eeebe5e50ce7352b8c00822cb3dbc31015dc86
Amauri1327 <amauri1816@hotmail.com>
2025-04-26 22:20:36 -0300

    update CalculoIR GenerationType

--- Backend/src/main/java/com/SCIR/impostoApi/model/CalculoIR.java
+++ Backend/src/main/java/com/SCIR/impostoApi/model/CalculoIR.java
@@ -10,7 +10,7 @@ import java.time.LocalDateTime;
 public class CalculoIR {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private Double rendaAnual;
     private Integer dependentes;
