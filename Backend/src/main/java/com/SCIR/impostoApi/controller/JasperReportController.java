2b3afdd1c70540af1fae89fc25fdface7e9c85c5
Amauri1327 <amauri1816@hotmail.com>
2025-04-28 10:52:25 -0300

    criacao metodo gerarRelatorio no JasperReportcontroller

--- Backend/src/main/java/com/SCIR/impostoApi/controller/JasperReportController.java
+++ Backend/src/main/java/com/SCIR/impostoApi/controller/JasperReportController.java
@@ -1,10 +1,25 @@
 package com.SCIR.impostoApi.controller;
 
+import com.SCIR.impostoApi.dto.CalculoIRDto;
+import com.SCIR.impostoApi.service.JasperReportService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/relatorio")
+@RequestMapping("/report")
 public class JasperReportController {
+
+    private final JasperReportService jasperReportService;
+
+    public JasperReportController(JasperReportService jasperReportService) {
+        this.jasperReportService = jasperReportService;
+    }
+
+    @PostMapping("/gerar-relatoio")
+    public void gerarRelatorio(@RequestBody CalculoIRDto calculoIRDto) {
+        this.jasperReportService.gerar(calculoIRDto);
+    }
 
 }
